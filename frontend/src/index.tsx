b852e8bcfa4ed1a027c06d39ddc40794fe9208c3
Guichana <guichana70@gmail.com>
2023-06-01 23:36:33 +0900

    webpack+react init

---
+++ frontend/src/index.tsx
@@ -0,0 +1,18 @@
+import '@styles/global.css'
+import { createRoot } from 'react-dom/client'
+import { AppRouter } from './app';
+import { RecoilRoot } from 'recoil';
+import { HashRouter } from 'react-router-dom';
+
+const app = document.createElement('div')
+document.body.appendChild(app)
+
+
+const root = createRoot(app);
+root.render(
+  <RecoilRoot>
+    <HashRouter>
+      <AppRouter />
+    </HashRouter>
+  </RecoilRoot>
+);

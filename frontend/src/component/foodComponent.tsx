25c74a5605393be27c71afe6b7b07eb662cb4d16
hanxhoon <hanxhoon@gmail.com>
2023-08-01 05:24:18 +0900

    1. foodComponent 구현중. 파일추가 시 알고리즘 조금 생각해서 기능추가할것.

--- frontend/src/component/foodComponent.tsx
+++ frontend/src/component/foodComponent.tsx
@@ -1,54 +1,70 @@
 import styles from '@styles/foodComponent.module.css'
 import { Pagination, Input } from 'antd'
 import { EditableTagComponent } from './editableTag_antd'
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 import { LeftOutlined, RightOutlined } from '@ant-design/icons'
+
+//state 인터페이스
+interface State {
+    LoadImage: { URL: string }[]
+}
 
 //todo
 //이미지 업로드 완료 이후, 편집버튼 눌르면 에디터 띄워서 편집 가능하게 ㄱㄱ.
 
 export function FoodComponent() {
+    const [state, setState] = useState<State>({
+        LoadImage: [
+            { URL: '/iconmonstr-plus-circle-lined.svg' },
+            { URL: '/iconmonstr-plus-circle-lined.svg' },
+            { URL: '/iconmonstr-plus-circle-lined.svg' },
+            { URL: '/iconmonstr-plus-circle-lined.svg' },
+            { URL: '/iconmonstr-plus-circle-lined.svg' },
+        ],
+    })
     const inputFileRef = useRef<HTMLInputElement | null>(null)
+    const imageRef = useRef<HTMLDivElement | null>(null)
 
     const onFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        //이미지 데이터타입 참조
+        //https://developer.mozilla.org/en-US/docs/Web/Media/Formats/Image_types
         if (!e.target.files) return
 
-        alert(e.target.files[0].name + ' ' + e.target.files[0].type)
-    }
+        //유사배열 -> 배열로 변환이후 필터링(이미지 파일이 아닌 파일 필터링)
+        var files = Array.from(e.target.files)
+        var filteringFiles = files.filter((element) =>
+            /^image\//.test(element.type),
+        )
 
-    const pageStyle: React.CSSProperties = {
-        backgroundImage: `url('/GrayWhiteCircle.svg')`,
-        backgroundRepeat: 'no-repeat',
-        backgroundPosition: 'center',
-        backgroundSize: 'contain',
-        backgroundOrigin: 'content-box',
-        padding: '5px',
-        height: '100%',
-    }
+        //알람 메시지
+        if (files.length !== filteringFiles.length)
+            alert('이미지파일이 아닌 파일은 필터링됩니다.')
 
-    const itemRender = (page: any, type: any, originalElement: any) => {
-        if (type === 'prev') {
-            console.log(page)
+        if (filteringFiles.length >= 6) {
+            alert('5개를 초과한 이미지는 무시됩니다.')
+            filteringFiles = filteringFiles.slice(0, 5)
         }
-        // console.log(type + page)
-        // if (type === 'prev') {
-        //     return (
-        //         <a>
-        //             <LeftOutlined />
-        //         </a>
-        //     )
-        // }
-        // if (type === 'next') {
-        //     return (
-        //         <a>
-        //             <RightOutlined />
-        //         </a>
-        //     )
-        // }
-        if (type === 'page') {
-            return <a style={pageStyle}></a>
-        }
-        return originalElement
+
+        const selectedFiles: string[] = filteringFiles.map((file) => {
+            return URL.createObjectURL(file)
+        })
+
+        var tempLoadImage = Array.from(state.LoadImage)
+
+        selectedFiles.map((element: string, index: number) => {
+            tempLoadImage[index].URL = element
+        })
+
+        setState({
+            LoadImage: tempLoadImage,
+        })
+
+        imageRef.current?.style.setProperty(
+            'background-image',
+            `url(${tempLoadImage[0].URL})`,
+        )
+
+        console.log(selectedFiles)
     }
 
     return (
@@ -59,21 +75,41 @@ export function FoodComponent() {
                     <label>0/5</label>
                 </div>
                 <div className={styles.RightContainer}>
-                    <div
-                        className={styles.img}
-                        style={{
-                            backgroundImage: `url(${'/iconmonstr-picture-thin.svg'})`,
-                        }}
-                        onClick={() => {
-                            inputFileRef.current?.click()
-                        }}
-                    ></div>
-                    <Pagination
-                        style={{ textAlign: 'center' }}
-                        defaultCurrent={1}
-                        total={50}
-                        itemRender={itemRender}
-                    />
+                    <div className={styles.ImageContainer}>
+                        <LeftOutlined className={styles.Button} />
+                        <div
+                            className={styles.img}
+                            ref={imageRef}
+                            style={{
+                                backgroundImage: `url(${'/iconmonstr-plus-circle-lined.svg'})`,
+                            }}
+                            onClick={() => {
+                                inputFileRef.current?.click()
+                            }}
+                        ></div>
+                        <RightOutlined className={styles.Button} />
+                    </div>
+                    <div className={styles.PaginationContainer}>
+                        {state.LoadImage.map((element) => {
+                            const onPageButtonclick = () => {
+                                imageRef.current?.style.setProperty(
+                                    'background-image',
+                                    `url(${element.URL})`,
+                                )
+                            }
+                            return (
+                                <div
+                                    className={`${styles.PageButton} ${
+                                        element.URL ===
+                                        '/iconmonstr-plus-circle-lined.svg'
+                                            ? styles.Page_None
+                                            : styles.Page_Selected
+                                    }`}
+                                    onClick={onPageButtonclick}
+                                ></div>
+                            )
+                        })}
+                    </div>
                 </div>
                 <input
                     ref={inputFileRef}

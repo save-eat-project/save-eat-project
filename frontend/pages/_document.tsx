2d18d49e558d6e2b38ed8d76b8291ab06972842d
guichana70 <guichana70@gmail.com>
2023-08-23 22:52:27 +0900

    페이지 및 로그인 예시 추가

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -1,9 +1,13 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript} from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html>
+      <Head>
+        <link rel="icon" href="/favicon-16x16.png" sizes="16x16" />
+        <link rel="icon" href="/favicon-32x32.png" sizes="32x32" />
+        <link rel="icon" href="/favicon-64x64.png" sizes="64x64" />
+      </Head>
       <body>
         <Main />
         <NextScript />
--- frontend/pages/test/[id].tsx
+++
@@ -1,8 +0,0 @@
-import { useRouter } from "next/router"
-
-export default function Home() {
-  const { query } = useRouter()
-  return (
-    <div>test:{query.id}</div>
-  )
-}

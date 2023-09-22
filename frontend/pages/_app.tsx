e08061f714d3fdd4bbe56320f86594235ff59563
guichana70 <guichana70@gmail.com>
2023-09-23 00:12:29 +0900

    queryclient 자동으로 refetch 하지 않도록 기본값 추가

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -3,7 +3,14 @@ import '@/assets/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { QueryClient, QueryClientProvider } from 'react-query'
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      refetchOnMount: false,
+    },
+  },
+})
 
 export default function App({ Component, pageProps }: AppProps) {
   return <QueryClientProvider client={queryClient}>
